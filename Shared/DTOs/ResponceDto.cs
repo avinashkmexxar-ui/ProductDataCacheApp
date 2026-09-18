7e13f52e819b7565e0f8b193379ef761d63074b3
Avinash <avinashk.mexxar@gmail.com>
2026-09-18 15:43:33 +0530

    feat(Backend): created Entites and DTOs

---
+++ Shared/DTOs/ResponceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.DTOs
+{
+    public sealed class ResponseDto<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int Code { get; set; }
+        public T? Data { get; set; }
+    }
+}
