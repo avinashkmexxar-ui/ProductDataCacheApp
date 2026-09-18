a079a14c05068e1f4e90ddeedf177bc1fd7f43b6
Avinash <avinashk.mexxar@gmail.com>
2026-09-18 19:19:25 +0530

    feat(backend): implemented Service Extension Method

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using API.Helpers;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using System.Reflection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, ConfigurationManager config)
+        {
+            services.AddScoped<IProductService, ProductService>(); 
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddHttpClient<IExternalProductClient, ExternalProductClient>((sp, client) =>
+            {
+                var baseUrl = "https://dummyjson.com/";
+                client.BaseAddress = new Uri(baseUrl);
+            });
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile<ProductMappingProfile>();
+            });
+            return services;
+        }
+
+    }
+}
